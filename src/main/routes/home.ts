0b0569c3388215db40001ad8b56516414c38ffc8
Adam John Ready <worldelephantseal@gmail.com>
2025-08-07 15:35:58 +0100

    Implement task list showing all tasks

--- src/main/routes/home.ts
+++ src/main/routes/home.ts
@@ -1,5 +1,5 @@
-import { Application } from 'express';
 import axios from 'axios';
+import { Application } from 'express';
 
 export default function (app: Application): void {
   app.get('/', async (req, res) => {
@@ -7,14 +7,16 @@ export default function (app: Application): void {
       // An example of connecting to the backend (a starting point)
       const response = await axios.get('http://localhost:4000/task');
       const tasks = response.data;
-      const formattedTasks = tasks.map((task:any) => ([
-        { text: task.title },
-        { text: task.description },
-        { text: task.status },
-        { text: (new Date(task.due_date)).toDateString() }
-      ]));
+      const formattedTasks = tasks.map(
+        (task: { title: string; description: string; status: string; due_date: string | number | Date }) => [
+          { text: task.title },
+          { text: task.description },
+          { text: task.status },
+          { text: new Date(task.due_date).toDateString() },
+        ]
+      );
 
-      res.render('home', { "tasks": formattedTasks });
+      res.render('home', { tasks: formattedTasks, tasks2: tasks });
     } catch (error) {
       console.error('Error making request:', error);
       res.render('home', {});
